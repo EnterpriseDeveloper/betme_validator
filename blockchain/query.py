844c0c17aa757fa7a9a8aeb57c8221ac725f1fde
Voroshilov Maksim <voroshilovmax90@gmail.com>
2026-02-19 22:47:28 +0100

    added proto. Everything works

---
+++ blockchain/query.py
@@ -0,0 +1,15 @@
+import os
+import grpc
+from bettery.events.v1 import query_pb2, query_pb2_grpc
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def fetch_events():
+    url = os.environ.get("COSMOS_RPC_URL")
+    channel = grpc.insecure_channel(url)
+    stub = query_pb2_grpc.QueryStub(channel)
+    request = query_pb2.QueryAllEventsRequest()
+    response = stub.ListEventsForValidator(request)
+    return response.events
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from blockchain.query import fetch_events
 
 app = FastAPI()
 
 
 @app.get("/ping")
 def read_root():
+    fetch_events()
     return {"status": "ok"}
